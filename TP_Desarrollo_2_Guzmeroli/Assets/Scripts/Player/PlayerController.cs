28e2db111eef080e884a0b9255d9a30ed5c6a013
FrostPowerX <emaguzme@hotmail.com>
2025-05-26 02:49:24 -0300

    Add more Logic for PlayerController

--- TP_Desarrollo_2_Guzmeroli/Assets/Scripts/Player/PlayerController.cs
+++ TP_Desarrollo_2_Guzmeroli/Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(WeaponController))]
 public class PlayerController : Character
 {
     [SerializeField] InputActionAsset inputActions;
     [SerializeField] LookProcces lookProcces;
+    [SerializeField] WeaponController wpController;
 
     [SerializeField] Vector2 sensivility;
 
     InputAction move;
     InputAction jump;
     InputAction look;
+    InputAction fire;
+    InputAction drop;
+    InputAction reload;
+    InputAction scroll;
 
     private void Awake()
     {
@@ -19,6 +25,10 @@ public class PlayerController : Character
         move = inputActions.FindAction("Move");
         jump = inputActions.FindAction("Jump");
         look = inputActions.FindAction("Look");
+        fire = inputActions.FindAction("Fire");
+        drop = inputActions.FindAction("Drop");
+        reload = inputActions.FindAction("Reload");
+        scroll = inputActions.FindAction("Scroll");
 
         jump.started += Jump;
 
@@ -27,8 +37,16 @@ public class PlayerController : Character
 
         look.performed += Look;
 
+        fire.started += FireWeapon;
+        drop.started += DropWeapon;
+        reload.started += ReloadWeapon;
+
+        scroll.performed += ScrollWeapon;
+
         Cursor.lockState = CursorLockMode.Locked;
     }
+
+
 
     private void Update()
     {
@@ -60,6 +78,35 @@ public class PlayerController : Character
     void CancelMove(InputAction.CallbackContext cont)
     {
         CancelForce();
+    }
+
+    void FireWeapon(InputAction.CallbackContext cont)
+    {
+        if (wpController)
+            wpController.FireWeapon();
+    }
+
+    void DropWeapon(InputAction.CallbackContext cont)
+    {
+        if (wpController)
+            wpController.DropWeapon();
+    }
+
+    void ReloadWeapon(InputAction.CallbackContext cont)
+    {
+        if (wpController)
+            wpController.ReloadWeapon();
+    }
+
+    void ScrollWeapon(InputAction.CallbackContext context)
+    {
+        if (wpController)
+        {
+            if (scroll.ReadValue<Vector2>().y == 1)
+                wpController.ScrollUp();
+            else if (scroll.ReadValue<Vector2>().y == -1)
+                wpController.ScrollDown();
+        }
     }
 
     void ActualizeDirectionMovement()
