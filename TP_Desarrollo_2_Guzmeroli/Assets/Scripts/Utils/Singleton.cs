e6ee89fce391a1144ffcddc83e04d2ad4e5c56c4
FrostPowerX <emaguzme@hotmail.com>
2025-05-26 02:48:39 -0300

    Compress Lines On Singleton

--- TP_Desarrollo_2_Guzmeroli/Assets/Scripts/Utils/Singleton.cs
+++ TP_Desarrollo_2_Guzmeroli/Assets/Scripts/Utils/Singleton.cs
@@ -4,13 +4,7 @@ public class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
     private static Singleton<T> instance = null;
 
-    public static T Instance
-    {
-        get
-        {
-            return (T)instance;
-        }
-    }
+    public static T Instance { get { return (T)instance; } }
 
     protected virtual void Initialize()
     {
