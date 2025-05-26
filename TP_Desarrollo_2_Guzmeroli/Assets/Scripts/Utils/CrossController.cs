74365401ea3fcba2129cf0e442952199224110f9
FrostPowerX <emaguzme@hotmail.com>
2025-05-26 02:50:00 -0300

    Add CrossController

---
+++ TP_Desarrollo_2_Guzmeroli/Assets/Scripts/Utils/CrossController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class CrossController : MonoBehaviour
+{
+    [SerializeField] GameObject defaultCross;
+    [SerializeField] GameObject interactCross;
+    [SerializeField] GameObject weaponCross;
+
+
+}
